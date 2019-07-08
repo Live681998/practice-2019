753447657ca4ce2da552e4f5a0ded0660a719193
Live681998 <ivanborovkov68@gmail.com>
2019-07-08 21:13:16 +0300

    lesson 8
    Inheritance

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Draw();
 
-            Console.ReadLine();
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
+
+            Employee e = new Employee();
+            e.age = 42;
+            e.pay = 30000;
         }
 
         
